f2b931846b301ce2f2cd5da8882f6bcda67427ba
Sophie Y <javachiphi@gmail.com>
2023-12-04 21:08:40 +0800

    create entryForm

---
+++ src/components/entryForm.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'; 
+import { Box, Sheet, Typography, Textarea, Button } from '@mui/joy';
+import axios from 'axios';
+import { BACKEND_URL } from '../constants';
+
+function getToday(){
+    const today = new Date();
+    const date = today.getDate();
+    const options = {month: "short"};
+    const year = today.getFullYear();
+    const formattedMonth = new Intl.DateTimeFormat("en-US", options).format(today)
+
+    const MonthDate = formattedMonth.concat(` ${date}, ${year}`)
+   /// const hourMin = 
+    
+    return formattedMonth.concat(` ${date}, ${year}`)
+}
+
+export default function EntryForm(){
+    const [observation, setObservation] = useState('');
+    const [solution, setSolution] = useState('');
+    const [data, setData] = useState({});
+
+    const handleChange = (e, type) => {
+        const value = e.target.value;
+        if(type === "observation"){
+            setObservation(value);
+            setData({"observation": value})
+        } else if(type === "solution"){
+            setSolution(value)
+        }
+    }
+    const handleSubmit =(e) => {
+        e.preventDefault();
+
+        axios.post(`${BACKEND_URL}/users/1/entries`, {
+            observation: observation, 
+            solution: solution
+        })
+        .then((response) => {
+            console.log('response', response.data);
+            setObservation('');
+            setSolution('');
+        })
+        .catch((error) =>{
+            console.log("error")
+        })
+    }
+    
+
+    return(
+        <Sheet sx={{display: "flex", flexDirection: 'column', gap: "10px", margin: "100px"}}>
+        <form onSubmit={handleSubmit}>
+        <Box sx={{display: "flex", alignContent:"center",  justifyContent: 'space-evenly' , alignItems: 'baseline'}}>
+            <Typography level="h1" sx={{marginTop: "50px", textAlign: "center"}}>
+            {getToday()}
+            </Typography>
+            <Button type="submit" variant="outlined" color="neutral" disabled={solution === '' && observation === ''}>Save</Button>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column'}}>
+            Observation
+            <Textarea
+                minRows={3}
+                value={observation}
+                onChange={(e) => handleChange(e, 'observation')}
+                placeholder={"Write your observation"}
+            />
+            Solution
+            <Textarea
+                minRows={3}
+                value={solution}
+                 onChange={(e) => handleChange(e, 'solution')}
+                placeholder={"What will you do about it?"}
+            />
+        </Box>
+        </form>
+        </Sheet>
+    )
+}
+
+
--- src/components/tagPage.js
+++ src/components/tagPage.js
@@ -11,16 +11,13 @@ export default function TagPage(){
     const [inputValue, setInputValue] = useState('');
     const [selectedTags, setSelectedTags] = useState([]); 
     const [originalData, setOriginalData] = useState([]);
-    const [selectedTagNames, setSelectedTagNames] = useState([]);
 
     useEffect(() => {
         axios.get(`${BACKEND_URL}/tags/users/1`)
         .then((response) => {
             const received = response.data;
-            // console.log(received);
             const getUserTags = received.map((data) => data.user_tags);
             const getTagIds = getUserTags.map((userTag) => userTag.tagId);
-            // console.log(getTagIds)
             setOriginalData(getTagIds);
             setSelectedTags(getTagIds);
          
@@ -46,10 +43,10 @@ export default function TagPage(){
         const idsToDelete = originalData.filter((id) => !newData.includes(id));
         const idsToAdd = newData.filter((id) => !originalData.includes(id));
 
-        //call post
-        axios.post(`${BACKEND_URL}/tags/users/1`, { idsToAdd })
+        //call post 
+        axios.post(`${BACKEND_URL}/tags/users/1`, { idsToAdd }) // fix user id to 'loggedin user'
           .then((response) => {
-            console.log("adding ids", response)
+            console.log("adding tags", response.data.message)
             // setSelectedTags()
           })
           .catch((error) => {
@@ -57,15 +54,15 @@ export default function TagPage(){
           });
         
         axios({
-            url: `${BACKEND_URL}/tags/users/1`,
+            url: `${BACKEND_URL}/tags/users/1`, // fix user id to 'loggedin user'
             method: 'delete',
             data: idsToDelete,
-          }).then((response) => console.log('hey', response))
+          }).then((response) => console.log('delete', response.data.message))
 
       };
 
     return(
-    <Sheet sx={{width: "1200px", display: "flex", flexDirection: 'column', gap: "10px", margin: "100px"}}>
+    <Sheet sx={{display: "flex", flexDirection: 'column', gap: "10px", margin: "100px"}}>
         {/* {selectedTags.map((id) => <div>{id}</div>)} */}
         <form onSubmit={handleSubmit}>
             <Box sx={{ display: 'flex', justifyContent: "space-between", mb: 2 }}>
