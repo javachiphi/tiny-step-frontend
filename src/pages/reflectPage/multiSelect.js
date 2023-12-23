c12acefcfd0f21d8a2137d1a7ffe3dcedc38fad9
Sophie Y <javachiphi@gmail.com>
2023-12-23 10:14:46 +0800

    refactorng 1: & show default value

--- src/pages/reflectPage/multiSelect.js
+++ src/pages/reflectPage/multiSelect.js
@@ -22,27 +22,16 @@ export default function MultiSelect({
   const [openModal, setOpenModal] = useState(false);
   const [newOption, setNewOption] = useState('');
   const [options, setOptions] = useState([]);
-  const [initialSelect, setInitialSelect] = useState([]);
  
   
   useEffect(() => {
     if(formattedData){
-      const filteredData = formattedData
-          .filter((item) => item.type === tagType)
-          .map(({ id, label }) => ({ id, label }));
-      setOptions(filteredData);
-  }
-
-  const selectValues
-        = defaultValues
-          .filter(item => item.type === tagType)
-          .map(item => ({ id: item.id, label: item.note }))  
-        //  .map(item => JSON.stringify(item));
-
-          console.log('selectValues', selectValues)
-
-  setInitialSelect(selectValues);
-
+            const filteredData = formattedData
+                .filter((item) => item.type === tagType)
+                .map(({ id, label }) => ({ id, label }));
+            setOptions(filteredData);
+        }
+    
   },[formattedData, tagType, defaultValues])
 
   const handleChange = (event, newValue) => {
@@ -53,6 +42,7 @@ export default function MultiSelect({
     const parsedValue = newValue.map(item => JSON.parse(item))
     const extractTagIds = parsedValue.filter(item => Number.isInteger(item.id)).map(item => item.id)
     const extractCreate = parsedValue.filter(item => item.action === 'create')
+    //for extracreate, if parsed string is not integer, then it is create (it would come with label)
 
     onTagsToCreateChange(extractCreate);
     onTagIdsChange(extractTagIds);
@@ -62,24 +52,28 @@ export default function MultiSelect({
   const handleAddOption = () => {
     if (newOption) {
       setOptions([...options, { id: `create-${newOption}`, label: newOption, action: 'create', tagType: tagType }]);
+      // for option send to only id
+      // for create send label // id can be also just newOption (label)
+      
       setNewOption('');
     }
     setOpenModal(false);
   };
-
-  
+  const initialIds = defaultValues && defaultValues.map(item => item.id)
   return (
     <>
-    <Select 
-      multiple
-      size="sm"
-      defaultValue={initialSelect}
-      onChange={handleChange}
-      renderValue={(selected) => 
-       { 
-        console.log('selected', selected)
-        return(
-        <Box sx={{ display: 'flex', gap: '0.25rem' }}>
+    { options.length > 0 && defaultValues.length > 0 ?
+    (
+      <>
+      <Select 
+        defaultValue={initialIds}
+        multiple
+        size="sm"
+        onChange={handleChange}
+        renderValue={(selected) => 
+         { 
+          return(
+          <Box sx={{ display: 'flex', gap: '0.25rem' }}>
           {selected
           .filter(option => option.label !== "+ Create an Option")
           .map((selectedOption, index) => {
@@ -90,47 +84,59 @@ export default function MultiSelect({
                 );
               })};
         </Box>
-      )
-       }
-      }
-      sx={{
-        minWidth: '15rem',
-      }}
-      slotProps={{
-        listbox: {
-          sx: {
-            width: '100%',
+        )
+         }}
+        sx={{
+          minWidth: '15rem',
+        }}
+        slotProps={{
+          listbox: {
+            sx: {
+              width: '100%',
+            },
           },
-        },
-      }}
-    >
-      {options && options.map((option) => (
-        <Option 
-          key={option.id}
-          value={JSON.stringify(option)}
-        >
-          {option.label}
-        </Option>
-      )
-      )}
-      <Option       
-        value={JSON.stringify({
-            id: "noOption",
-            label: "+ Create an Option"
-          })}
-        >
-            + Create an Option
-        </Option>
-    </Select>
-    <OptionModal
-      open={openModal}
-      setOpen={setOpenModal}
-      newOption={newOption}
-      setNewOption={setNewOption}
-      handleAddOption={handleAddOption} 
-    />
+        }}
+      >
+        {options && options.map((option, index) => {
+        return(
+          <Option 
+            key={index}
+            value={option.id}
+          >
+            {option.label} {option.id}
+          </Option>
+        )
+      }
+        )}
+        <Option       
+          value={"noOption"}
+          >
+              + Create an Option
+          </Option>
+      </Select>
+      <OptionModal
+        open={openModal}
+        setOpen={setOpenModal}
+        newOption={newOption}
+        setNewOption={setNewOption}
+        handleAddOption={handleAddOption} 
+      />
+      </>
+    ) : <div>loading...</div>
+  }
     </>
   );
 }
 
 
+
+
+
+
+// to do to switch from json stringify to just {id: a , label: x} 
+// goal is match default value
+// 1. set value = item.id ;  display item.label 
+//2. set default value to item id (ensure that both option value and default value match) in terms of string or id. check 
+//3. handle add new option -> when add new option, set value to {id: create-{index}, label: newOption}
+//4. handle change -> CREATE: if id is not integer then should send those 'label information' along with tagType (received) -format data item = {label: newOption, type: tagType}
+  // add -> just simply send ids (check entryTable handleSave function)
