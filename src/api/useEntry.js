30d4ed7af4b857d77772ffa83281063431f476db
Sophie Y <javachiphi@gmail.com>
2023-12-25 14:58:42 +0800

    implement mui navigation

--- src/api/useEntry.js
+++ src/api/useEntry.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchData } from './apiService';
 import { useAuthToken } from "../components/useAuthToken";
 
@@ -10,7 +10,7 @@ export default function useEntry(entryId) {
   const jwtToken = useAuthToken();
 
   useEffect(() => {
-    if(jwtToken && entryId !== null){
+    if(jwtToken && entryId !== (null || undefined)){
     fetchData(`entries/${entryId}`, jwtToken)
       .then((data) => {
         setEntry(data);
