b87e99f07fc8dfbbb7cedd90e816ad8f1f4ac326
Sophie Y <javachiphi@gmail.com>
2023-12-27 11:46:38 +0800

    part 2: mobile friendly table & button alignment

--- src/pages/reflectPage/index.js
+++ src/pages/reflectPage/index.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import { fetchData } from "../../api/apiService";
 import { useAuthToken } from "../../components/useAuthToken";
 import TableEntryList from "./entryTable";
-import { Button, Sheet, Typography } from "@mui/joy";
+import { Button, Sheet, Typography, Card } from "@mui/joy";
 import Add from '@mui/icons-material/Add';
 import {Link } from "react-router-dom";
 import { MoreHoriz } from "@mui/icons-material";
@@ -34,21 +34,41 @@ export default function ReflectPage(){
         <div>
             <h1 className="text-center">Reflect Page</h1>
             <Sheet>
+                <HowToDescription />
+                <div style={{display: "flex", flexDirection: "column", gap: "20px"}}>
+                <Button 
+                    sx={{alignSelf: "flex-end"}}
+                    component={Link} 
+                    to="/create" 
+                    startDecorator={<Add />}
+                >
+                    Write
+                </Button>
+                <Card variant="outlined">
+                    <div className="table-container">
+                    <TableEntryList 
+                        data={entries} 
+                        setDataChanged={setDataChanged}
+                    />
+                    </div>
+                </Card>
+                </div>
+            </Sheet>
+        </div>
+    )
+}
+
+
+function HowToDescription(){
+    return(
+        <>
             <Typography sx={{fontWeight: 700}}>How To</Typography>
             <ol>
-                <li>Click on <MoreHoriz/> to revisit observation to detect a pattern such as natural tendency, bias, or mental space.</li>
+                <li>Click on <MoreHoriz/> to revisit observation to detect a pattern such as mental tendency, bias, or mental space.</li>
                 <li>Think about how you'd approach it differently next time when similar situation arises. </li>
                 <li>Come up with a solution in 1 sentence. think clearly</li>
                 <li>tag your mental tendency, approach and situation. </li>
             </ol>
-            <Button 
-            sx={{ float: 'right'}}
-            component={Link} to="/create" startDecorator={<Add />}>Write</Button>
-            <TableEntryList 
-                data={entries} 
-                setDataChanged={setDataChanged}
-            />
-            </Sheet>
-        </div>
+        </>
     )
 }
