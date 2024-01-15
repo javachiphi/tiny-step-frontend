03d700100cd0c45c1bfb17fd28b297066a4c9337
Sophie Y <javachiphi@gmail.com>
2024-01-15 15:24:34 +0800

    linting

--- src/components/protectedRoute.js
+++ src/components/protectedRoute.js
@@ -2,12 +2,9 @@ import React from 'react'
 import { LinearProgress } from '@mui/joy'
 import { useAuth0 } from '@auth0/auth0-react'
 import LandingPage from '../pages/landingPage'
-import { useNavigate } from 'react-router-dom'
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, isLoading } = useAuth0()
-  const navigate = useNavigate()
-
 
   if (isLoading) {
     return <LinearProgress color='success' />
@@ -15,7 +12,7 @@ const ProtectedRoute = ({ children }) => {
 
   if (!isAuthenticated) {
     return <LandingPage />
-  } 
+  }
 
   return children
 }
--- src/context/snackbarProvider.js
+++ src/context/snackbarProvider.js
@@ -1,31 +1,31 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext } from 'react'
 
-const SnackbarContext = createContext();
+const SnackbarContext = createContext()
 
 const SnackbarProvider = ({ children }) => {
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false)
 
   const showSnackbar = () => {
-    setSnackbarOpen(true);
-  };
+    setSnackbarOpen(true)
+  }
 
   const hideSnackbar = () => {
-    setSnackbarOpen(false);
-  };
+    setSnackbarOpen(false)
+  }
 
   const contextValue = {
     snackbarOpen,
     showSnackbar,
     hideSnackbar,
-  };
+  }
 
   return (
     <SnackbarContext.Provider value={contextValue}>
       {children}
     </SnackbarContext.Provider>
-  );
-};
+  )
+}
 
-export const useSnackbar = () => useContext(SnackbarContext);
+export const useSnackbar = () => useContext(SnackbarContext)
 
-export default SnackbarProvider;
+export default SnackbarProvider
